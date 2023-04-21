08114c92dc9caaaeffc9ebfd6d7a087191ae4e73
Michael Egbinola <michaelmarketing25@gmail.com>
2023-04-21 03:11:42 -0400

    Working with Routes and Route

--- puremike-clothing/src/App.js
+++ puremike-clothing/src/App.js
@@ -1,40 +1,23 @@
-import CategoryItem from "./components/category-item-component";
-import "./categories-container.styles.scss";
+import Home from "./routes/home-component";
+import Navigation from "./routes/navigation-component";
+import { Routes, Route } from "react-router-dom";
+
+const Shop = () => {
+  return (
+    <div>
+      <div>I am in the shop page</div>
+    </div>
+  );
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ puremike-clothing/src/routes/navigation-component.jsx
@@ -0,0 +1,32 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as Puremikesvg } from "../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div>
+        <div className="navigation">
+          <Link className="logo-container" to="/">
+            <Puremikesvg className="logo" />
+          </Link>
+          <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+              SHOP
+            </Link>
+            <Link className="nav-link" to="/shop">
+              CONTACT
+            </Link>
+            <Link className="nav-link" to="/shop">
+              SIGN IN
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
